3934967b6c471f91fd725ac48bf0fc096c66bf36
JulienCamus38 <camusjulien40@gmail.com>
2018-02-26 09:17:03 +0100

    First make

--- binomial_pricer.cpp
+++ binomial_pricer.cpp
@@ -1,1 +1,22 @@
-// TODO
+/* ----------------------------------------------------------------
+**
+**  PROGRAM:
+**
+**  PURPOSE:
+**
+**  USAGE:
+**
+** ----------------------------------------------------------------
+*/
+
+#include "binomial_pricer.hpp"
+#include "util.hpp"
+#include <err_code.h>
+#include "device_picker.hpp"
+
+int main(int argc, char *argv[])
+{
+    // TODO
+
+    return EXIT_SUCCESS;
+}
--- binomial_pricer.hpp
+++ binomial_pricer.hpp
@@ -1,1 +1,29 @@
-// TODO
+/* ----------------------------------------------------------------
+**
+**  Include file for the binomial pricer
+**
+** ----------------------------------------------------------------
+*/
+
+#ifndef __BIN_PRICER
+#define __BIN_PRICER
+
+#define CL_USE_DEPRECATED_OPENCL_1_2_APIS   /* clCreateCommandQueue
+                                               is deprecated: avoids
+                                               warning */
+#define __CL_ENABLE_EXCEPTIONS
+#include "cl.hpp"
+
+// ----------------------------------------------------------------
+//  Functions from ./common
+// ----------------------------------------------------------------
+extern double wtime();   /* returns time since some fixed past point
+                            (wtime.c) */
+
+// ----------------------------------------------------------------
+//  Constants
+// ----------------------------------------------------------------
+#define SUCCESS  1
+#define FAILURE  0
+
+#endif
